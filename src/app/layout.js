9079843a90c3bff808ee8323741efbe54260a7bf
Timmotal <timmortal44@yahoo.com>
2024-09-07 22:00:54 +0100

    FIX attach logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        
+        <link
+          rel="icon"
+          href="../../public/footer-icons/recode-circle.png"
+          sizes="any"
+        />
         {children}
       </body>
     </html>
