78296cfa885a1d964a0cc957b6f3edcca5002a16
Timmotal <timmortal44@yahoo.com>
2024-09-08 23:00:44 +0100

    bring in the NAV

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@
 import Goal from "@/components/goal/Goal";
 import HeroCards from "@/components/herocards/HeroCards";
 import Hero from "@/components/hero/Hero";
+import Nav from "@/components/nav/Nav";
 
 export default function Home() {
  
