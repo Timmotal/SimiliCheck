00e9929d5b42dbe9e49c1e8463a8827731b1d490
Timmotal <timmortal44@yahoo.com>
2024-09-08 23:00:27 +0100

    ready the hero

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 import Goal from "@/components/goal/Goal";
 import HeroCards from "@/components/herocards/HeroCards";
+import Hero from "@/components/hero/Hero";
 
 export default function Home() {
  
