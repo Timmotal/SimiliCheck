f33c863b81d7989167e9481a6b965b6813ec59c3
Timmotal <timmortal44@yahoo.com>
2024-09-09 21:51:14 +0100

    FIX ui remake for works section

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import HeroCards from "@/components/herocards/HeroCards";
 import Hero from "@/components/hero/Hero";
 import Nav from "@/components/nav/Nav";
 import Solution from "@/components/solution/Solution";
+import Works from "@/components/works/Works";
 
 export default function Home() {
  
