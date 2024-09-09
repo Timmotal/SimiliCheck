0fdecf093cc5d8c05ceccaebc7437252284d3a0a
Timmotal <timmortal44@yahoo.com>
2024-09-09 21:50:41 +0100

    FEATURE add in the solution comp to the main

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import Goal from "@/components/goal/Goal";
 import HeroCards from "@/components/herocards/HeroCards";
 import Hero from "@/components/hero/Hero";
 import Nav from "@/components/nav/Nav";
+import Solution from "@/components/solution/Solution";
 
 export default function Home() {
  
