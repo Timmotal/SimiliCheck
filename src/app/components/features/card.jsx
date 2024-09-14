6816c8bcf96f8d00b4de8eb76abee59b23ffc9e9
Timmotal <timmortal44@yahoo.com>
2024-09-14 22:29:08 +0100

    set particular state for inViewFeature

--- src/app/components/features/card.jsx
+++ src/app/components/features/card.jsx
@@ -3,6 +3,11 @@ import classNames from "classnames";
 import { useFeatureStore } from "./store";
 
 const FeatureCard = ({ children, id, imageUrl }) => {
+  const inViewFeature = useFeatureStore((state) => state.inViewFeature);
+
+ 
+};
+
 
 
 
