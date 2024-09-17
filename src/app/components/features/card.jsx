1dd6f3d6fb582a0d92a5bde0efe6623dc4d69f0f
Timmotal <timmortal44@yahoo.com>
2024-09-17 22:29:44 +0100

    make parent card

--- src/app/components/features/card.jsx
+++ src/app/components/features/card.jsx
@@ -6,7 +6,18 @@ const FeatureCard = ({ children, id, imageUrl }) => {
   const inViewFeature = useFeatureStore((state) => state.inViewFeature);
 
   return (
-
+    <div
+      className={classNames(
+        "absolute inset-0 h-full w-full rounded-2xl bg-gradient-to-br transition-opacity",
+        inViewFeature === id
+          ? "active-card opacity-100"
+          : "pointer-events-none opacity-0"
+      )}
+    >
+    
+   
+ 
+    </div>
   );
 };
 
