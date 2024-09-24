051668d9dd17171fb964a70a2a4133c92d3c14d2
Timmotal <timmortal44@yahoo.com>
2024-09-24 21:25:01 +0100

    feat title for title comp

--- src/app/components/features/title.jsx
+++ src/app/components/features/title.jsx
@@ -2,3 +2,11 @@ import classNames from "classnames";
 import { useInView } from "framer-motion";
 import React, { useEffect, useRef } from "react";
 import { useFeatureStore } from "./store";
+
+const FeatureTitle = ({ children, id }) => {
+
+  return (
+
+  );
+};
+
