02f7ea71f7a7ae63fc2bde6df7c5085159a5b655
Timmotal <timmortal44@yahoo.com>
2024-10-03 22:42:36 +0100

    add CTA line for community

--- src/app/components/footer/Footer.js
+++ src/app/components/footer/Footer.js
@@ -3,6 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center gap-8">
+      <h3 className="primary text-[28px] lg:text-[32px] text-darkPink  w-full px-4 text-center">
+        Join Our Community
+      </h3>
   
   
       <div>
