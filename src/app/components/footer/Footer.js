36a2d3012bfc77c0ee2ef850d5590fe57365d28f
Timmotal <timmortal44@yahoo.com>
2024-10-03 22:42:20 +0100

    make outer container

--- src/app/components/footer/Footer.js
+++ src/app/components/footer/Footer.js
@@ -2,6 +2,15 @@ import React from "react";
 
 const Footer = () => {
   return (
+    <div className="w-full flex flex-col justify-center items-center gap-8">
+  
+  
+      <div>
 
+    
+      </div>
+
+    
+    </div>
   );
 };
