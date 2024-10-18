770e8e2fabe195f9a2ea6ef781200c219b1005ad
Timmotal <timmortal44@yahoo.com>
2024-10-18 22:13:52 +0100

    get class up for fly

--- src/app/components/funding/Funding.jsx
+++ src/app/components/funding/Funding.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const Funding = () => {
   return (
+    <div className="flex flex-col  justify-center items-center md:gap-[60px]  gap-5 p-6 mb-10">
    
+
+
+        </div>
      
       </div>
     </div>
