65d9e60b97a49791741c80715b06f78365cf870f
Timmotal <timmortal44@yahoo.com>
2024-10-20 11:30:07 +0100

    mke max width to enhance user experience

--- src/app/components/funding/Funding.jsx
+++ src/app/components/funding/Funding.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Funding = () => {
   return (
     <div className="flex flex-col  justify-center items-center md:gap-[60px]  gap-5 p-6 mb-10">
-   
+      <div className="flex flex-col text-center md:gap-6 gap-5 w-full max-w-[612px]">
+    
+      
+      </div>
 
 
         </div>
