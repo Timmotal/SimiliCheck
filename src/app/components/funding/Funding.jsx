8428d1b4749b5b62e7db8543b96acd0f71a2bcc1
Timmotal <timmortal44@yahoo.com>
2024-11-08 20:33:36 +0000

    center funds secion to the front

--- src/app/components/funding/Funding.jsx
+++ src/app/components/funding/Funding.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Funding = () => {
   return (
     <div className="flex flex-col  justify-center items-center md:gap-[60px]  gap-5 p-6 mb-10">
-    
+      <div className="flex flex-col text-center md:gap-6 gap-5 w-full max-w-[612px]">
+   
+   
+      </div>
+
       <div className="flex flex-col md:flex-row justify-center items-center gap-8 lg:gap-20  md:pb-20 lg:mt-10">
         <div>
           <img src="/consciousFunding.png" alt="" className="" />
