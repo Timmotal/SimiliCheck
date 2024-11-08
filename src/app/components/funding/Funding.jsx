1d3331203363df1c22dde87f4a6bb1cad2d3ee0b
Timmotal <timmortal44@yahoo.com>
2024-11-08 20:34:06 +0000

    include text

--- src/app/components/funding/Funding.jsx
+++ src/app/components/funding/Funding.jsx
@@ -5,7 +5,7 @@ const Funding = () => {
     <div className="flex flex-col  justify-center items-center md:gap-[60px]  gap-5 p-6 mb-10">
       <div className="flex flex-col text-center md:gap-6 gap-5 w-full max-w-[612px]">
         <h3 className="primary text-[26px] lg:text-[32px] text-darkPink ">
-      
+          Conscious Funding
         </h3>
    
       </div>
