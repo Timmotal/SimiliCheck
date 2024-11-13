b1b94ff484732d91c6530a3209cb0e0fedf127a9
Timmotal <timmortal44@yahoo.com>
2024-11-13 23:35:51 +0000

    bring in full maxer

--- src/app/components/goal/Goal.jsx
+++ src/app/components/goal/Goal.jsx
@@ -6,7 +6,10 @@ const Goal = () => {
       className="flex flex-col-reverse md:flex-row justify-center items-center gap:20 md:gap-8  mlg:gap-[120px] p-6 my-10  md:pl-10"
       id="goal"
     >
-    
+      <div className="w-full lg:max-w-[590px] flex flex-col md:items-start justify-center items-center lg:justify-start lg:items-start   gap-6 md:w-full md:max-w-[300px]  ">
+     
+       
+      </div>
       <div className="w-[297px] h-[286px] lg:w-[450px] lg:h-[450px] mt-14">
         <img src="/goal.png" alt="goal.png" className="h-full w-full" />
       </div>
