485123bfb8b34e595642cd59241dc71eff3718a8
Timmotal <timmortal44@yahoo.com>
2024-11-13 06:48:38 +0000

    make goal ID rlevant

--- src/app/components/goal/Goal.jsx
+++ src/app/components/goal/Goal.jsx
@@ -2,7 +2,13 @@ import React from "react";
 
 const Goal = () => {
   return (
-   
+    <div
+      className="flex flex-col-reverse md:flex-row justify-center items-center gap:20 md:gap-8  mlg:gap-[120px] p-6 my-10  md:pl-10"
+      id="goal"
+    >
+    
+    
+    </div>
   );
 };
 
