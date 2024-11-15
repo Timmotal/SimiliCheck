50320c79aaa214d1ef3da5261a250a6228a269a7
Timmotal <timmortal44@yahoo.com>
2024-11-15 18:18:52 +0000

    structure wireframe for hero

--- src/app/components/hero/Hero.jsx
+++ src/app/components/hero/Hero.jsx
@@ -3,7 +3,10 @@ import Chatbot from "../Chatbot";
 
 const Hero = () => {
   return (
+    <div className="relative  xlg:h-[74vh]  w-screen text-lightGray flex flex-col justify-center items-center pt-32 lg:pt-28 xlg:pt-20">
+
   
+    </div>
   );
 };
 
