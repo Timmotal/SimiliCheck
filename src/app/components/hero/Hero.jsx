fa1207bef175d777c3159be329ef81c5983e1f44
Timmotal <timmortal44@yahoo.com>
2024-11-19 21:35:20 +0000

    add max fill bg

--- src/app/components/hero/Hero.jsx
+++ src/app/components/hero/Hero.jsx
@@ -4,9 +4,12 @@ import Chatbot from "../Chatbot";
 const Hero = () => {
   return (
     <div className="relative  xlg:h-[74vh]  w-screen text-lightGray flex flex-col justify-center items-center pt-32 lg:pt-28 xlg:pt-20">
+    <div className="w-full object-fill lg:max-w-[1200px] md:px-4 relative xlg:max-w-[1450px]">
 
-  
     </div>
+
+  </div>
+  
   );
 };
 
