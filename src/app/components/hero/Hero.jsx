82d38da63c4f47db6e889dbbe73e75387e4a7e24
Timmotal <timmortal44@yahoo.com>
2024-11-19 21:35:33 +0000

    include image for max fill

--- src/app/components/hero/Hero.jsx
+++ src/app/components/hero/Hero.jsx
@@ -5,6 +5,11 @@ const Hero = () => {
   return (
     <div className="relative  xlg:h-[74vh]  w-screen text-lightGray flex flex-col justify-center items-center pt-32 lg:pt-28 xlg:pt-20">
     <div className="w-full object-fill lg:max-w-[1200px] md:px-4 relative xlg:max-w-[1450px]">
+      <img
+        src="/hero-bg.png"
+        alt="bgImg"
+        className="w-full h-[260px] md:h-[440px] lg:h-full object-cover"
+      />
 
     </div>
 
