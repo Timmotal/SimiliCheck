b0af1ef3028d2e3be6594a7b1ae4b08892cc1570
Timmotal <timmortal44@yahoo.com>
2024-11-26 21:55:58 +0000

    flex on main to be full

--- src/app/components/herocards/HeroCards.jsx
+++ src/app/components/herocards/HeroCards.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const HeroCards = () => {
   return (
-
+    <div className="flex flex-col md:flex-row md:flex-wrap gap-6 justify-center items-center h-full  mt-8 lg:mt-10 xlg:gap-14">
+   
+     
+    
+    </div>
   );
 };
 
