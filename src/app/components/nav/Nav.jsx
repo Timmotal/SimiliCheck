90fe93ea38289f5c136a7684cc20719aed64c2b6
Timmotal <timmortal44@yahoo.com>
2024-12-05 21:35:27 +0000

    Fix inconsistent font sizes for desktop and mobile menus

--- src/app/components/nav/Nav.jsx
+++ src/app/components/nav/Nav.jsx
@@ -15,7 +15,7 @@ const Nav = () => {
     <nav className="fixed z-10 top-0 left-0 text-white p-4 xlg:px-20 w-screen  h-14 uppercase">
       <div className=" bg-white text-[#232323] flex justify-between items-center rounded-full p-3 w-full">
         <div className="h-10">
-     
+          <img src="/logo.png" alt="" className="h-full" />
         </div>
    
      
