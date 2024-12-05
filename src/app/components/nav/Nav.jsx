423e29da17179c8ff3d0b8a1a14e96c3e321288b
Timmotal <timmortal44@yahoo.com>
2024-12-05 21:34:35 +0000

    Add hover effect to navigation links for better UX

--- src/app/components/nav/Nav.jsx
+++ src/app/components/nav/Nav.jsx
@@ -13,7 +13,12 @@ const Nav = () => {
 
   return (
     <nav className="fixed z-10 top-0 left-0 text-white p-4 xlg:px-20 w-screen  h-14 uppercase">
-
+      <div className=" bg-white text-[#232323] flex justify-between items-center rounded-full p-3 w-full">
+   
+   
+     
+  
+      </div>
       {/* Responsive menu dropdown */}
   
     </nav>
