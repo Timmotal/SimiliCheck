3926d12f4de53425013b0e1e0abd160271fd49f3
Timmotal <timmortal44@yahoo.com>
2024-12-13 20:35:30 +0000

    Add container div and set layout styles for Roadmap

--- src/app/components/roadmap/RoadMap.jsx
+++ src/app/components/roadmap/RoadMap.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Roadmap = () => {
   return (
-    
+    <div className="w-full  flex flex-col  justify-center items-center gap-12 px-4 mlg:px-[110px]  pb-0 ">
+   
+    </div>
   );
 };
 
