6e96fc77be2c0d23dc1ce784d0e1a2be48502c0a
Timmotal <timmortal44@yahoo.com>
2024-12-13 20:35:51 +0000

    Style the Roadmap title with color and font size

--- src/app/components/roadmap/RoadMap.jsx
+++ src/app/components/roadmap/RoadMap.jsx
@@ -4,7 +4,9 @@ const Roadmap = () => {
   return (
     <div className="w-full  flex flex-col  justify-center items-center gap-12 px-4 mlg:px-[110px]  pb-0 ">
       <div className="flex flex-col justify-center items-center gap-12">
-   
+        <h3 className="primary text-[28px] mlg:text-[32px] text-darkPink text-center">
+          OUR ROADMAP Over the next year we aim to
+        </h3>
   
 
         {/* img */}
