cb4416d8b980cee84223ba8853a6b00f901c9825
Timmotal <timmortal44@yahoo.com>
2024-12-13 20:35:42 +0000

    Create heading for the Roadmap section

--- src/app/components/roadmap/RoadMap.jsx
+++ src/app/components/roadmap/RoadMap.jsx
@@ -3,7 +3,13 @@ import React from "react";
 const Roadmap = () => {
   return (
     <div className="w-full  flex flex-col  justify-center items-center gap-12 px-4 mlg:px-[110px]  pb-0 ">
+      <div className="flex flex-col justify-center items-center gap-12">
    
+  
+
+        {/* img */}
+   
+      </div>
     </div>
   );
 };
