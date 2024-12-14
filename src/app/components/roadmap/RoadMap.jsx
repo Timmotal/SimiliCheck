7717fb9a402f0a1f5c8c2f92d3ee0669ee8d8c4c
Timmotal <timmortal44@yahoo.com>
2024-12-14 21:18:58 +0000

    Style roadmap step 3 with rounded corners

--- src/app/components/roadmap/RoadMap.jsx
+++ src/app/components/roadmap/RoadMap.jsx
@@ -13,7 +13,9 @@ const Roadmap = () => {
               <p className="text-darkGray text-[14px] mlg:text-[15px] primary px-[23px] py-[17px]  border-[3px]  border-darkPink bg-skyBlue rounded-full ">
                 1
               </p>
-        
+              <span className="w-full max-w-[208px]">
+            
+              </span>
             </div>
          
          
