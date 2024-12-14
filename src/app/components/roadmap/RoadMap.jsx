bc9f9dc7cc953c81784da9377961dcf1f5b5e109
Timmotal <timmortal44@yahoo.com>
2024-12-14 21:17:52 +0000

    Add roadmap step 1 with content and styling

--- src/app/components/roadmap/RoadMap.jsx
+++ src/app/components/roadmap/RoadMap.jsx
@@ -7,7 +7,10 @@ const Roadmap = () => {
         <h3 className="primary text-[28px] mlg:text-[32px] text-darkPink text-center">
           OUR ROADMAP Over the next year we aim to
         </h3>
+        <div className="flex  mlg:flex-col justify-center items-center SpaceMonoRegular text-[14px] mlg:text-[15px] !text-lightGray w-full border-collapse   text-center mlg:text-start ">
   
+       
+        </div>
 
         {/* img */}
    
