9b57d14304668089a558245b605a1f0f69777281
Timmotal <timmortal44@yahoo.com>
2024-12-20 23:58:53 +0000

    Styled image container to be responsive with different screen sizes

--- src/app/components/solution/Solution.jsx
+++ src/app/components/solution/Solution.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Solution = () => {
   return (
     <div className="flex flex-col md:flex-row justify-center items-center mlg:gap-[120px] gap-[60px] my-10  p-6">
-     
+      <div className="w-[278px] h-[320px] lg:w-[470px] lg:h-[540px] mt-10 lg:mt-0">
+       
+      </div>
      
     </div>
   );
