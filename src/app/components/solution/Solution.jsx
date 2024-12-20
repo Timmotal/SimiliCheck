7223d3035a9bd9460f923a19e409c2d200647e5b
Timmotal <timmortal44@yahoo.com>
2024-12-20 23:58:40 +0000

    Added img element for solution image with alt text

--- src/app/components/solution/Solution.jsx
+++ src/app/components/solution/Solution.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Solution = () => {
   return (
-    
+    <div className="flex flex-col md:flex-row justify-center items-center mlg:gap-[120px] gap-[60px] my-10  p-6">
+     
+     
+    </div>
   );
 };
 
