cc5f2c5d302cdca69221643f46db662ebabd0471
Timmotal <timmortal44@yahoo.com>
2024-12-21 22:55:42 +0000

    Created second div for text content and added heading

--- src/app/components/solution/Solution.jsx
+++ src/app/components/solution/Solution.jsx
@@ -4,7 +4,7 @@ const Solution = () => {
   return (
     <div className="flex flex-col md:flex-row justify-center items-center mlg:gap-[120px] gap-[60px] my-10  p-6">
       <div className="w-[278px] h-[320px] lg:w-[470px] lg:h-[540px] mt-10 lg:mt-0">
-       
+        <img src="/solution.png" alt="solution.png" className="h-full w-full" />
       </div>
      
     </div>
