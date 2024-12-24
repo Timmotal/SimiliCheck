7f632595099de5b838972957a9190899763e380d
Timmotal <timmortal44@yahoo.com>
2024-12-24 19:02:44 +0000

    Add heading for 'Our Launch Partners' section

--- src/app/components/team/Team.jsx
+++ src/app/components/team/Team.jsx
@@ -3,7 +3,13 @@ import React from "react";
 const Team = () => {
   return (
     <div className="flex flex-col justify-center items-center pb-20">
-   
+      <div className="w-full flex flex-col justify-center items-center gap-8 mt-10">
+       
+
+     
+      
+       
+      </div>
     </div>
   );
 };
