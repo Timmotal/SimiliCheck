29f0cae1c1ee9ba75eae93f042755b5a5e2ed134
Timmotal <timmortal44@yahoo.com>
2024-12-27 17:04:28 +0000

    Add flex container for logos

--- src/app/components/team/Team.jsx
+++ src/app/components/team/Team.jsx
@@ -7,7 +7,9 @@ const Team = () => {
       <h3 className="primary text-[28px] lg:text-[32px] text-darkPink  w-full px-4 text-center">
           Our Launch Partners
         </h3>
-       
+        <p className="SpaceMonoRegular  text-[14px] lg:text-[15px] text-lightGray w-full max-w-[440px] lg:max-w-[716px] text-center px-4">
+   
+        </p>
 
      
       
