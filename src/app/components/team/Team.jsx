da498da4a2dc55c92bb4044bc4222e82372d1bd7
Timmotal <timmortal44@yahoo.com>
2024-12-27 17:04:07 +0000

    Add styling for heading text

--- src/app/components/team/Team.jsx
+++ src/app/components/team/Team.jsx
@@ -5,7 +5,7 @@ const Team = () => {
     <div className="flex flex-col justify-center items-center pb-20">
       <div className="w-full flex flex-col justify-center items-center gap-8 mt-10">
       <h3 className="primary text-[28px] lg:text-[32px] text-darkPink  w-full px-4 text-center">
-        
+          Our Launch Partners
         </h3>
        
 
