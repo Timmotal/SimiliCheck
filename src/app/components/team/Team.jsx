9161554d1764084a2ac7ea5ca6fffcef837f8b27
Timmotal <timmortal44@yahoo.com>
2024-12-27 17:03:56 +0000

    Add paragraph with description text

--- src/app/components/team/Team.jsx
+++ src/app/components/team/Team.jsx
@@ -4,6 +4,9 @@ const Team = () => {
   return (
     <div className="flex flex-col justify-center items-center pb-20">
       <div className="w-full flex flex-col justify-center items-center gap-8 mt-10">
+      <h3 className="primary text-[28px] lg:text-[32px] text-darkPink  w-full px-4 text-center">
+        
+        </h3>
        
 
      
